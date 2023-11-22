68660914cb5da11e9cde5eba9848b48f933f5a51
JabalnurIT <jabalnur.it@gmail.com>
2023-11-23 06:22:08 +0700

    update feat: clean and fix

--- client/client.py
+++ client/client.py
@@ -3,19 +3,16 @@ import time
 
 sio = socketio.Client()
 
-# Meminta nama pengguna dan kunci dari pengguna
 username = input("Enter your username: ")
+encryption_key = input("Enter the key to encrypt the message: ")
 
-# Daftar pengguna yang terhubung
 connected_users = []
-# Daftar Pesan yang diterima
 received_messages = []
 
 @sio.event
 def connect():
     global connected_users
     print("Connected to server")
-    # Mengirim nama pengguna ke server untuk mengatur kunci rahasia
     sio.emit('set_username', {'username': username})
 
 @sio.event
@@ -27,10 +24,8 @@ def receive_message(data):
     sender_sid = data['sender_sid']
     encrypted_text = data['encrypted_text']
     global received_messages
-    # Menambahkan pesan ke daftar pesan yang diterima
     received_messages.append({'sender_sid': sender_sid, 'text': encrypted_text})
 
-# sio.emit('user_list', {'users': user_list}, room=sid)
 @sio.event
 def open_message(data):
     print(f"Received Message from {data['username']}: {data['text']}")
@@ -41,7 +36,6 @@ def user_list(data):
     for user in data['users']:
         if user not in connected_users:
             connected_users.append(user)
-    # connected_users = data['users']
 
 if __name__ == '__main__':
     server_url = 'http://localhost:8888'
@@ -50,7 +44,6 @@ if __name__ == '__main__':
     
     while True:
         sio.emit('get_user_list')
-        # Memasukkan pilihan untuk mengirim, menerima, atau keluar
         print("\nMenu:")
         print("1. Send Message")
         print("2. Receive Message")
@@ -74,10 +67,6 @@ if __name__ == '__main__':
 
                 text_to_send = input("Enter message to send: ")
                 
-                # Meminta kunci dari pengguna untuk mengenkripsi pesan
-                encryption_key = input("Enter the key to encrypt the message: ")
-                
-                # Send encrypted message
                 sio.emit('send_message', {'text': text_to_send, 'key': encryption_key, 'recipient_sid': recipient_sid})
                 
                 print("Send Again? (y/n)")
@@ -99,27 +88,16 @@ if __name__ == '__main__':
                 except (ValueError, IndexError):
                     print("Invalid choice. Please enter a valid number.")
                     continue
-
-
-                # Meminta kunci dari pengguna untuk mendekripsi pesan
-                decryption_key = input("Enter the key to decrypt the message: ")
                 
-                # Menampilkan total pesan yang diterima dari semua pengguna
+
                 print(f"Total Messages Received: {len(received_messages)}")
-                # Menampilkan total pesan yang diterima dari semua pengguna
-                print(f"Total Messages Received: {len(received_messages)}")
-
-
-
-                # Menampilkan pesan yang diterima dari pengguna yang dipilih
-                print(f"Messages from {sender_user['username']}:")
-                for message in received_messages:
-                    if message['sender_sid'] == sender_user['sid']:
-                        # Decrypt the received message
-                        sio.emit('get_message', {'text': message['text'], 'key': decryption_key, 'sender_username': sender_user['username']})
-                        # sio.emit()
-                        # decrypted_text = decrypt(message['text'], decryption_key)
-                        # print(f"Received Message from {sender_userp['username']}: {decrypted_text}")
+                
+                if len(received_messages) != 0:
+                    decryption_key = input("Enter the key to decrypt the message: ")
+                    print(f"Messages from {sender_user['username']}:")
+                    for message in received_messages:
+                        if message['sender_sid'] == sender_user['sid']:
+                            sio.emit('get_message', {'text': message['text'], 'key': decryption_key, 'sender_username': sender_user['username']})
                 
                 time.sleep(1)
                 print("Open Again? (y/n)")
--- server/server.py
+++ server/server.py
@@ -10,15 +10,11 @@ utils = Utils()
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 
-
-# Fungsi enkripsi DES
-def encrypt(text, key):
+def generateKey(key):
     key = key.upper()
 
-    # Key generation
     key = utils.hex2bin(key)
 
-    # --parity bit drop table
     keyp = [57, 49, 41, 33, 25, 17, 9,
             1, 58, 50, 42, 34, 26, 18,
             10, 2, 59, 51, 43, 35, 27,
@@ -28,16 +24,13 @@ def encrypt(text, key):
             14, 6, 61, 53, 45, 37, 29,
             21, 13, 5, 28, 20, 12, 4]
 
-    # Getting 56-bit key from 64-bit using the parity bits
     key = des.permute(key, keyp, 56)
 
-    # Number of bit shifts
     shift_table = [1, 1, 2, 2,
                 2, 2, 2, 2,
                 1, 2, 2, 2,
                 2, 2, 2, 1]
 
-    # Key- Compression Table: Compression of key from 56 bits to 48 bits
     key_comp = [14, 17, 11, 24, 1, 5,
                 3, 28, 15, 6, 21, 10,
                 23, 19, 12, 4, 26, 8,
@@ -47,25 +40,26 @@ def encrypt(text, key):
                 44, 49, 39, 56, 34, 53,
                 46, 42, 50, 36, 29, 32]
 
-    # Splitting
     left = key[0:28]  # rkb for RoundKeys in binary
     right = key[28:56]  # rk for RoundKeys in hexadecimal
 
     rkb = []
     rk = []
     for i in range(0, 16):
-        # Shifting the bits by nth shifts by checking from shift table
         left = des.shift_left(left, shift_table[i])
         right = des.shift_left(right, shift_table[i])
 
-        # Combination of left and right string
         combine_str = left + right
 
-        # Compression of key from 56 to 48 bits
         round_key = des.permute(combine_str, key_comp, 48)
 
         rkb.append(round_key)
         rk.append(utils.bin2hex(round_key))
+
+    return rk, rkb
+
+def encrypt(text, key):
+    rk, rkb = generateKey(key)
 
     pt_all = utils.string_to_hexadecimal(text).upper()
     pt_chunks = [pt_all[i:i + 16] for i in range(0, len(pt_all), 16)]
@@ -82,61 +76,8 @@ def encrypt(text, key):
 
     return cipher_text_all
 
-# Fungsi dekripsi DES
 def decrypt(text, key):
-    key = key.upper()
-
-    # Key generation
-    key = utils.hex2bin(key)
-
-    # --parity bit drop table
-    keyp = [57, 49, 41, 33, 25, 17, 9,
-            1, 58, 50, 42, 34, 26, 18,
-            10, 2, 59, 51, 43, 35, 27,
-            19, 11, 3, 60, 52, 44, 36,
-            63, 55, 47, 39, 31, 23, 15,
-            7, 62, 54, 46, 38, 30, 22,
-            14, 6, 61, 53, 45, 37, 29,
-            21, 13, 5, 28, 20, 12, 4]
-
-    # Getting 56-bit key from 64-bit using the parity bits
-    key = des.permute(key, keyp, 56)
-
-    # Number of bit shifts
-    shift_table = [1, 1, 2, 2,
-                2, 2, 2, 2,
-                1, 2, 2, 2,
-                2, 2, 2, 1]
-
-    # Key- Compression Table: Compression of key from 56 bits to 48 bits
-    key_comp = [14, 17, 11, 24, 1, 5,
-                3, 28, 15, 6, 21, 10,
-                23, 19, 12, 4, 26, 8,
-                16, 7, 27, 20, 13, 2,
-                41, 52, 31, 37, 47, 55,
-                30, 40, 51, 45, 33, 48,
-                44, 49, 39, 56, 34, 53,
-                46, 42, 50, 36, 29, 32]
-
-    # Splitting
-    left = key[0:28]  # rkb for RoundKeys in binary
-    right = key[28:56]  # rk for RoundKeys in hexadecimal
-
-    rkb = []
-    rk = []
-    for i in range(0, 16):
-        # Shifting the bits by nth shifts by checking from shift table
-        left = des.shift_left(left, shift_table[i])
-        right = des.shift_left(right, shift_table[i])
-
-        # Combination of left and right string
-        combine_str = left + right
-
-        # Compression of key from 56 to 48 bits
-        round_key = des.permute(combine_str, key_comp, 48)
-
-        rkb.append(round_key)
-        rk.append(utils.bin2hex(round_key))
+    rk, rkb = generateKey(key)
     
     cipher_text_hexa_all = utils.string_to_hexadecimal(text).upper()
     cipher_text_hexa_all_chunks = [cipher_text_hexa_all[i:i + 16] for i in range(0, len(cipher_text_hexa_all), 16)]
@@ -157,7 +98,6 @@ def decrypt(text, key):
 
     return text_all
 
-# Daftar klien yang terhubung
 clients = {}
 
 @sio.event
@@ -167,7 +107,7 @@ def connect(sid, environ):
 @sio.event
 def disconnect(sid):
     print(f"Client {sid} disconnected")
-    # Menghapus informasi tentang klien yang terputus
+
     clients.pop(sid, None)
 
 @sio.event
@@ -177,10 +117,8 @@ def send_message(sid, data):
     key = data['key']
     recipient_sid = data['recipient_sid']
     
-    # Encrypt the message before sending
     encrypted_text = encrypt(text, key)
     
-    # Kirim pesan terenkripsi ke klien penerima
     for client_sid, client_info in clients.items():
         if client_sid == recipient_sid:
             sio.emit('receive_message', {'sender_sid': sender_sid, 'encrypted_text': encrypted_text}, room=client_sid)
@@ -188,18 +126,14 @@ def send_message(sid, data):
 
 @sio.event
 def set_username(sid, data):
-    # Mendapatkan nama pengguna dari klien
     username = data.get('username')
     if username:
-        # Menambahkan informasi klien ke dalam daftar
         clients[sid] = {'sid': sid, 'username': username}
         print(f"Client {sid} set username to {username}")
 
 @sio.event
 def get_user_list(sid):
-    # Mengirim daftar pengguna dan sid yang terhubung ke klien
     user_list = [{'username': client_info['username'], 'sid': client_info['sid']} for client_sid, client_info in clients.items()]
-    # Print user list
     user_send = []
     for user in user_list:
         if user['sid'] != sid:
@@ -208,7 +142,6 @@ def get_user_list(sid):
 
 @sio.event
 def get_message(sid, data):
-
     decrypted_text = decrypt(data['text'], data['key'])
     sio.emit('open_message', {'username': data['sender_username'], 'text':decrypted_text}, room=sid)
 
